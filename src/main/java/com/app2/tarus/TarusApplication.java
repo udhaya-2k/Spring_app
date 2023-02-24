df830cb4ec67c0f5d8dabf539d999476e267210f
udhaya-2k <udhayakumarasamy18@gmail.com>
2023-02-24 15:39:03 +0530

    adding src

---
+++ src/main/java/com/app2/tarus/TarusApplication.java
@@ -0,0 +1,13 @@
+package com.app2.tarus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app2/tarus/TarusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app2.tarus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
